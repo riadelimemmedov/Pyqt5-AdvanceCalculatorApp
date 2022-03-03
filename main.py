4993519f2bf271735f48e0d997eb89d05327f760
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2022-03-04 00:01:53 +0400

    addoperatorbutton

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class Calculator(QtWidgets.QMainWindow):
         self.show()
     
     def UI(self):
-        ##########Entry Field################
+        #!#########Entry Field################
         self.entry_box = QtWidgets.QLineEdit(self)
         self.entry_box.resize(335,30)
         self.entry_box.setAlignment(Qt.AlignRight)
@@ -21,7 +21,7 @@ class Calculator(QtWidgets.QMainWindow):
         self.entry_box.setText('O')
         self.entry_box.move(20,30)
         
-        #########Number Button############
+        #!########Number Button############
         btn_number = []
         for i in range(1,10):
             i = QtWidgets.QPushButton(str(i),self)
@@ -35,6 +35,56 @@ class Calculator(QtWidgets.QMainWindow):
             for j in range(0,3):
                 btn_number[btn_index].move(25+j*90,70+i*70)#move=>first parametr x,second parametr y,horizontal window
                 btn_index+=1
+
+        #!########Operator Button########
+        btn_operator = []
+        for i in range(0,4):
+            i = QtWidgets.QPushButton(self)
+            i.resize(70,40)
+            i.setStyleSheet('background-color:white')
+            i.setFont(QFont('Arial',15))
+            btn_operator.append(i)
+        
+        btn_operator[0].setText('+')
+        btn_operator[1].setText('-')
+        btn_operator[2].setText('*')
+        btn_operator[3].setText('/')
+        
+        for i in range(0,len(btn_operator)):
+            btn_operator[i].move(290,70+i*70)
+        
+        #!########Other Buttons########
+        btn_zero = QtWidgets.QPushButton('0',self)
+        btn_zero.setStyleSheet('background-color:white')
+        btn_zero.setFont(QFont('Arial',20))
+        btn_zero.resize(250,40)
+        btn_zero.move(25,280)
+        
+        btn_clear = QtWidgets.QPushButton('C',self)
+        btn_clear.setStyleSheet('background-color:white')
+        btn_clear.setFont(QFont('Arial',20))
+        btn_clear.resize(70,40)
+        btn_clear.move(25,340)
+        
+        btn_dot = QtWidgets.QPushButton('.',self)
+        btn_dot.setStyleSheet('background-color:white')
+        btn_dot.setFont(QFont('Arial',15))
+        btn_dot.resize(70,40)
+        btn_dot.move(110,340)
+        
+        btn_equal = QtWidgets.QPushButton('=',self)
+        btn_equal.setStyleSheet('background-color:white')
+        btn_equal.setFont(QFont('Arial',15))
+        btn_equal.resize(70,40)
+        btn_equal.move(200,340)
+        
+        btn_delete = QtWidgets.QPushButton(self)
+        btn_delete.setStyleSheet('background-color:white')
+        btn_delete.setFont(QFont('Arial',15))
+        btn_delete.setIcon(QIcon('arrow.png'))
+        btn_delete.resize(70,40)
+        btn_delete.move(290,340)
+        
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
